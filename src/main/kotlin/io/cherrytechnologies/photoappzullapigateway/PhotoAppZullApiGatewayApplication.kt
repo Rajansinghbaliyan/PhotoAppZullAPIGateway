bf078167a7f0d9c6dd4c857666afd4e39db2a9a8
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-20 11:56:28 +0530

    config changes

--- src/main/kotlin/io/cherrytechnologies/photoappzullapigateway/PhotoAppZullApiGatewayApplication.kt
+++ src/main/kotlin/io/cherrytechnologies/photoappzullapigateway/PhotoAppZullApiGatewayApplication.kt
@@ -2,8 +2,12 @@ package io.cherrytechnologies.photoappzullapigateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
 class PhotoAppZullApiGatewayApplication
 
 fun main(args: Array<String>) {
