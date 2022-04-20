726ee6f95b74af986b09e67aa391a40c06498a30
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-20 11:47:44 +0530

    Initial commit

---
+++ src/main/kotlin/io/cherrytechnologies/photoappzullapigateway/PhotoAppZullApiGatewayApplication.kt
@@ -0,0 +1,11 @@
+package io.cherrytechnologies.photoappzullapigateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PhotoAppZullApiGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<PhotoAppZullApiGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/io/cherrytechnologies/photoappzullapigateway/PhotoAppZullApiGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.cherrytechnologies.photoappzullapigateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PhotoAppZullApiGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
