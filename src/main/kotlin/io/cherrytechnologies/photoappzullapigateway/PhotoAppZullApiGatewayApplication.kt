02d65418e4f42248f33dcf1e3f80169e2500d123
Rajan <53229081+Rajansinghbaliyan@users.noreply.github.com>
2022-04-20 14:02:20 +0530

    remove the zuul and replaced it with cloud gateway

--- src/main/kotlin/io/cherrytechnologies/photoappzullapigateway/PhotoAppZullApiGatewayApplication.kt
+++ src/main/kotlin/io/cherrytechnologies/photoappzullapigateway/PhotoAppZullApiGatewayApplication.kt
@@ -3,11 +3,9 @@ package io.cherrytechnologies.photoappzullapigateway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy
 class PhotoAppZullApiGatewayApplication
 
 fun main(args: Array<String>) {
--- src/test/kotlin/io/cherrytechnologies/photoappzullapigateway/PhotoAppZullApiGatewayApplicationTests.kt
+++ src/test/kotlin/io/cherrytechnologies/photoappzullapigateway/PhotoAppZullApiGatewayApplicationTests.kt
@@ -3,10 +3,10 @@ package io.cherrytechnologies.photoappzullapigateway
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+
 class PhotoAppZullApiGatewayApplicationTests {
 
-    @Test
+
     fun contextLoads() {
     }
 
